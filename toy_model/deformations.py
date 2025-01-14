82eba112cf2e8a5f65f79f7085f700b972662659
Donald Huber-Youmans <donaldhuberyoumans@gmail.com>
2025-01-14 23:35:58 +0000

    logically optimized classes

--- toy_model/deformations.py
+++ toy_model/deformations.py
@@ -1,23 +1,29 @@
 import torch
+from torch import nn
 from linalg import *
-       
-class Linear():
-    def __init__(self):
-        pass
+from utils import grab
+
+class Linear(nn.Module):
+    def __init__(self,a0,n):
+        super().__init__()
+
+        assert a0.shape[-2] == 2*n + 2, f"expected (vector, real) dim {2*n + 2}, got {a0.shape[-2]}"
+
+        self.a = nn.Parameter(a0)
+        self.identity = torch.eye(2*n + 2)
+        
 
     # DEFORM 
-    def complexify(self,phi,a):
+    def complexify(self,phi):
         X = phi
-        dim_R = X.shape[-2]
-        id = torch.eye(dim_R)
 
         inner_XX = X.transpose(-1,-2) @ X
         outer_XX = X @ X.transpose(-1,-2)
-        inner_aX = a.transpose(-1,-2) @ X
-        outer_aX = a @ X.transpose(-1,-2) 
+        inner_aX = self.a.transpose(-1,-2) @ X
+        outer_aX = self.a @ X.transpose(-1,-2) 
 
         # DEFORMATION / COMPLEXIFICATION
-        Y = a - inner_aX / inner_XX * X 
+        Y = self.a - inner_aX / inner_XX * X 
 
         inner_YY = Y.transpose(-1,-2) @ Y
         lam = torch.sqrt(1 + inner_YY) 
@@ -25,9 +31,9 @@ class Linear():
         tildeZ = X * lam + 1j*Y 
 
         # JACOBIAN
-        M = - id * inner_aX / inner_XX - outer_aX / inner_XX +  2*inner_aX * outer_XX / inner_XX**2
+        M = - self.identity * inner_aX / inner_XX - outer_aX / inner_XX +  2*inner_aX * outer_XX / inner_XX**2
 
-        J = id*lam + (M @ Y) @ X.transpose(-1,-2) / lam + 1j*M
+        J = self.identity*lam + (M @ Y) @ X.transpose(-1,-2) / lam + 1j*M
 
         det = torch.det(J) # (batch, #particles) -> multiply
         detJ = det / lam.squeeze(dim=(-1,-2))**2 
@@ -37,22 +43,29 @@ class Linear():
         assert len(detJ.shape) == 1, f'detJ has wrong dim: {detJ.shape} but must be 1' 
 
         return tildeZ, detJ
+    
+    def get_param(self):
+        return grab(self.a)
 
-class Homogeneous():
-    def __init__(self):
-        pass
+class Homogeneous(nn.Module):
+    def __init__(self,a0,n):
+        super().__init__()
+
+        assert a0.shape[-1] == n**2 + 2*n
+
+        self.a = nn.Parameter(a0)
+        self.n = n # CP(n)
+        self.identity = torch.eye(2*n + 2)
+
+        self.su_n = LieSU(n+1)
 
     # DEFORM 
-    def complexify(self,phi,a):
+    def complexify(self,phi):
+        assert phi.shape[-2] == 2*self.n + 2, f"phi has wrong (vector, real) dim. Expected {2*self.n + 2}, got {phi.shape[-2]}"
+
         X = phi
-        dim_R = X.shape[-2]
-        dim_C = dim_R // 2
-        id = torch.eye(dim_R)
 
-        if not hasattr(self, 'su_n'):
-            self.su_n = LieSU(dim_C)
-
-        a_ = rho(1j*self.su_n.embed(a)).cdouble() # assuming Hermitian su_n generators 
+        a_ = rho(1j*self.su_n.embed(self.a)) # assuming Hermitian su(n) generators 
 
         outer_XX = X @ X.transpose(-1,-2)
 
@@ -65,48 +78,15 @@ class Homogeneous():
         tildeZ = X * lam + 1j*Y 
 
         # JACOBIAN
-        J = id*lam - outer_XX @ (a_ @ a_) / lam + 1j*a_
+        J = self.identity*lam - outer_XX @ (a_ @ a_) / lam + 1j*a_
         det = torch.det(J) # (samples, #particles) -> multiply 
-        detJ = det / lam.squeeze(dim=(-1,-2))**2 
+        detJ = det / lam.squeeze(dim=(-1,-2))**2 # extra factor from delta fct
 
         detJ = torch.prod(detJ,dim=-1) # total Jacobian (samples,)
 
         assert len(detJ.shape) == 1, f'detJ has wrong dim: {detJ.shape} but must be 1' 
     
         return tildeZ, detJ
-
-# class Torus():
-#     def __init__(self):
-#         pass
-
-#     # DEFORM 
-#     def complexify(self,phi,a):
-#         X = phi
-#         dimR = X.shape[-2]
-#         dimC = dimR // 2
-#         id = torch.eye(dimR)
-
-#         su_n = LieSU(dimC)
-#         a_ = rho(su_n.embed(a)).cdouble()
-
-#         outer_XX = X @ X.transpose(-1,-2)
-
-#         # DEFORMATION / COMPLEXIFICATION
-#         Y = a_ @ X 
-#         inner_YY = Y.transpose(-1,-2) @ Y
-
-#         lam = torch.sqrt(1 + inner_YY)
-
-#         tildeZ = X * lam + 1j*Y 
-
-#         # JACOBIAN
-#         J = id*lam + (a_ @ a_) @ outer_XX / lam - 1j*a_
-#         det = torch.det(J) # (samples, #particles) -> multiply
-#         detJ = det / lam.squeeze(dim=(-1,-2))**2 
-
-#         detJ = torch.prod(detJ,dim=-1) # total Jacobian (samples,)
-
-#         assert len(detJ.shape) == 1, f'detJ has wrong dim: {detJ.shape} but must be 1' 
     
-#         return tildeZ, detJ
-
+    def get_param(self):
+        return grab(self.a)
