1d482a205be0efb84754bd57391bb1c35e7b10d7
Donald Huber-Youmans <donaldhuberyoumans@gmail.com>
2025-01-14 23:29:51 +0000

    added losses

--- toy_model/losses.py
+++ toy_model/losses.py
@@ -3,13 +3,19 @@ import torch
 def loss(obs):
     return obs.var()
 
-def loss_re(obs):
+def rloss(obs):
     assert obs.is_complex(), "expect complex input"
     return obs.real.var()
 
-def loss_im(obs):
+def iloss(obs):
     assert obs.is_complex, "expect complex input"
     return obs.imag.var()
 
 def logloss(obs):
     return torch.log(obs.var())
+
+def rlogloss(obs):
+    return torch.log(obs.real.var())
+
+def ilogloss(obs):
+    return torch.log(obs.imag.var())
