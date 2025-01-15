078c76b7d48b933302ff7a544cb1fa1f2f6de4b5
Donald Huber-Youmans <donaldhuberyoumans@gmail.com>
2025-01-15 16:58:11 +0000

    added safe_grab
    checks for Inf or NaN

--- toy_model/utils.py
+++ toy_model/utils.py
@@ -1,2 +1,14 @@
+import numpy as np
+
 def grab(x): 
     return x.detach().cpu().numpy()
+
+def safe_grab(x): 
+    array = x.detach().cpu().numpy()
+
+    if np.any(np.isnan(array)):
+        print("Warning: NaN!")
+
+    if np.any(np.isinf(array)):
+        print("Warning: Inf detected in tensor!")
+    return array
