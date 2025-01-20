89a0161179200aaec28a27275ea364291389950a
Donald Huber-Youmans <donaldhuberyoumans@gmail.com>
2025-01-20 20:05:17 +0000

    added timer (context manager)

--- toy_model/utils.py
+++ toy_model/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 def grab(x): 
     return x.detach().cpu().numpy()
@@ -12,3 +13,22 @@ def safe_grab(x):
     if np.any(np.isinf(array)):
         print("Warning: Inf detected in tensor!")
     return array
+
+import time
+
+class Timer:
+    """
+    A simple context manager for timing code execution.
+    """
+    def __init__(self,msg):
+        self.msg = msg
+
+    def __enter__(self):
+        self.t0 = time.time()
+        return self
+
+    def __exit__(self,exc_type, exc_val, exc_tb):
+        end_time = time.time()
+        t1 = end_time - self.t0
+        print(self.msg,f"took {t1:.4f}s")
+        return False  
