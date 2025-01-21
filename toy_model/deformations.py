d558c0998a09144300c8d74c87f8374d1fc41c19
Donald Huber-Youmans <donaldhuberyoumans@gmail.com>
2025-01-21 20:00:23 +0000

    added complexify_single
    deform along single field

--- toy_model/deformations.py
+++ toy_model/deformations.py
@@ -4,7 +4,7 @@ from linalg import *
 from utils import *
 
 class Linear(nn.Module):
-    def __init__(self,a0,n,mode="0D"):
+    def __init__(self,a0,n,spacetime="0D",safe_grab=True):
         super().__init__()
 
         assert a0.shape[-2] == 2*n + 2, f"expected (vector, real) dim {2*n + 2}, got {a0.shape[-2]}"
@@ -12,7 +12,8 @@ class Linear(nn.Module):
         self.a = nn.Parameter(a0)
         self.identity = torch.eye(2*n + 2)
 
-        self.mode = mode
+        self.spacetime = spacetime
+        self.safe_grab = safe_grab
         
 
     # DEFORM 
@@ -40,9 +41,9 @@ class Linear(nn.Module):
         det = torch.det(J) # (samples, particles) -> multiply
         detJ = det / lam.squeeze(dim=(-1,-2))**2 
 
-        if self.mode == "0D":
+        if self.spacetime == "0D":
             detJ = torch.prod(detJ,dim=-1) # total Jacobian (samples,)
-        elif self.mode == "2D":
+        elif self.spacetime == "2D":
             Lx = detJ.shape[-2]
             Ly = detJ.shape[-1]
             detJ = torch.prod(detJ.view(-1,Lx*Ly),dim=-1) # total Jacobian (samples,)
@@ -52,10 +53,10 @@ class Linear(nn.Module):
         return tildeZ, detJ
     
     def get_param(self):
-        return grab(self.a)
+        return grab(self.a,safe=self.safe_grab)
 
 class Homogeneous(nn.Module):
-    def __init__(self,a0,n,mode="0D"):
+    def __init__(self,a0,n,spacetime="0D",safe_grab=True):
         super().__init__()
 
         assert a0.shape[-1] == n**2 + 2*n
@@ -66,7 +67,8 @@ class Homogeneous(nn.Module):
 
         self.su_n = LieSU(n+1)
 
-        self.mode = mode
+        self.spacetime = spacetime
+        self.safe_grab = safe_grab
 
     # DEFORM 
     def complexify(self,phi):
@@ -93,9 +95,9 @@ class Homogeneous(nn.Module):
 
         detJ = det / (lam.squeeze(dim=(-1,-2))**2) # incl. extra factor from delta fct
 
-        if self.mode == "0D":
+        if self.spacetime == "0D":
             detJ = torch.prod(detJ,dim=-1) # total Jacobian (samples,)
-        elif self.mode == "2D":
+        elif self.spacetime == "2D":
             Lx = detJ.shape[-2]
             Ly = detJ.shape[-1]
             detJ = torch.prod(detJ.view(-1,Lx*Ly),dim=-1) # total Jacobian (samples,)
@@ -104,5 +106,41 @@ class Homogeneous(nn.Module):
     
         return tildeZ, detJ
     
+    # DEFORM SINGLE FIELD
+    def complexify_single(self,phi):
+        assert phi.shape[-2] == 2*self.n + 2, f"phi has wrong (vector, real) dim. Expected {2*self.n + 2}, got {phi.shape[-2]}"
+        assert self.spacetime == "0D", f"This deformation exists only for the toy model"
+
+        X = phi[:,0]
+        XS = phi[:,1]
+
+        dtype = X.dtype # I don't like this!
+
+        a_ = rho(1j*self.su_n.embed(self.a[0])).to(dtype) # assuming Hermitian su(n) generators 
+
+        outer_XX = X @ X.transpose(-1,-2)
+
+        # DEFORMATION / COMPLEXIFICATION
+        Y = a_ @ X 
+        inner_YY = Y.transpose(-1,-2) @ Y
+
+        lam = torch.sqrt(1 + inner_YY)
+
+        tildeZ = X * lam + 1j*Y 
+
+        tildeZ = torch.stack((tildeZ,XS),dim=1)
+        assert tildeZ.shape == phi.shape, "tilde Z has wrong shape!"
+
+        # JACOBIAN
+        J = self.identity*lam - outer_XX @ (a_ @ a_) / lam + 1j*a_
+        det = torch.det(J) # (samples, ) 
+        
+
+        detJ = det / (lam.squeeze(dim=(-1,-2))**2) # incl. extra factor from delta fct
+
+        assert len(detJ.shape) == 1, f'detJ has wrong dim: {detJ.shape} but must be 1' 
+    
+        return tildeZ, detJ
+    
     def get_param(self):
-        return grab(self.a)
+        return grab(self.a,safe=self.safe_grab)
