72d76537a4f1c5f8b34395994c9128bad5945bf3
Donald Huber-Youmans <donaldhuberyoumans@gmail.com>
2025-02-16 15:03:05 +0000

    added comments

--- lattice/src/deformations.py
+++ lattice/src/deformations.py
@@ -4,7 +4,7 @@ from linalg import *
 from utils import *
 
 class NNHom(nn.Module):
-    def __init__(self,neural_net,n,spacetime="2D",safe_grab=False):
+    def __init__(self,neural_net,n,safe_grab=False):
         super().__init__()
 
         self.nn = neural_net
@@ -18,14 +18,13 @@ class NNHom(nn.Module):
 
         self.su_n = LieSU(n+1)
 
-        self.spacetime = spacetime
         self.safe_grab = safe_grab
 
     # DEFORM 
     def complexify(self, phi, mask):
         assert phi.shape[-2] == 2*self.n + 2, f"phi has wrong (vector, real) dim. Expected {2*self.n + 2}, got {phi.shape[-2]}"
 
-        X = phi # (samples, Lx,Ly,dim,1)
+        X = phi # (samples, Lx,Ly,dim_R,1)
         dtype = X.dtype 
 
         self.a = self.nn(mask).permute(1,2,0)
@@ -48,49 +47,10 @@ class NNHom(nn.Module):
 
         detJ = det / (lam.squeeze(dim=(-1,-2))**2) # incl. extra factor from delta fct
 
-        if self.spacetime == "0D":
-            detJ = torch.prod(detJ,dim=-1) # total Jacobian (samples,)
-        elif self.spacetime == "2D":
-            Lx = X.shape[-4]
-            Ly = X.shape[-3]
+        Lx = X.shape[-4]
+        Ly = X.shape[-3]
 
-            detJ = torch.prod(detJ.view(-1,Lx*Ly),dim=-1) # total Jacobian (samples,)
-
-        assert len(detJ.shape) == 1, f'detJ has wrong dim: {detJ.shape} but must be 1' 
-    
-        return tildeZ, detJ
-    
-    # DEFORM SINGLE FIELD
-    def complexify_single(self,phi):
-        assert phi.shape[-2] == 2*self.n + 2, f"phi has wrong (vector, real) dim. Expected {2*self.n + 2}, got {phi.shape[-2]}"
-        assert self.spacetime == "0D", f"This deformation exists only for the toy model"
-
-        X = phi[:,0]
-        XS = phi[:,1]
-
-        dtype = X.dtype # I don't like this!
-
-        a_ = rho(1j*self.su_n.embed(self.a[0])).to(dtype) # assuming Hermitian su(n) generators 
-
-        outer_XX = X @ X.transpose(-1,-2)
-
-        # DEFORMATION / COMPLEXIFICATION
-        Y = a_ @ X 
-        inner_YY = Y.transpose(-1,-2) @ Y
-
-        lam = torch.sqrt(1 + inner_YY)
-
-        tildeX = X * lam + 1j*Y 
-
-        tildeZ = torch.stack((tildeX,XS),dim=1)
-        assert tildeZ.shape == phi.shape, "tilde Z has wrong shape!"
-
-        # JACOBIAN
-        J = self.identity*lam - outer_XX @ (a_ @ a_) / lam + 1j*a_
-        det = torch.det(J) # (samples, ) 
-        
-
-        detJ = det / (lam.squeeze(dim=(-1,-2))**2) # incl. extra factor from delta fct
+        detJ = torch.prod(detJ.view(-1,Lx*Ly),dim=-1) # total Jacobian (samples,)
 
         assert len(detJ.shape) == 1, f'detJ has wrong dim: {detJ.shape} but must be 1' 
     
